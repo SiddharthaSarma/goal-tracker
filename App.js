44c826d8d191bbcc2091c2df6d1dfe76adafe224
Siddhartha <sidduinevitable@gmail.com>
2019-04-25 22:32:02 +0530

    initial commit :tada:

---
+++ App.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { Constants } from 'expo';
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+import Chart from './components/Chart';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Chart />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+    padding: 8,
+  }
+});
---
+++ components/Chart.js
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {
+  LineChart
+} from 'react-native-chart-kit';
+import { View, Text, Dimensions } from 'react-native';
+import { Constants } from 'expo';
+
+export default class Chart extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Bezier Line Chart</Text>
+        <LineChart
+          data={{
+            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={Dimensions.get('window').width - 15} // from react-native
+          height={220}
+          yAxisLabel={'$'}
+          chartConfig={{
+            backgroundColor: '#000',
+            backgroundGradientFrom: '#000',
+            backgroundGradientTo: '#000',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          bezier
+          style={{
+            marginVertical: 8,
+            borderRadius: 16,
+          }}
+        />
+      </View>
+    );
+  }
+}
